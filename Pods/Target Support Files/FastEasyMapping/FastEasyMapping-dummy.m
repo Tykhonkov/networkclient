c7acaebc12bd55d1473a40febfec0948f6c6df11
Ilya Tihonkov <illya.tihonkov@yalantis.com>
2017-01-18 13:16:48 +0200

    removed pods folder

--- Pods/Target Support Files/FastEasyMapping/FastEasyMapping-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FastEasyMapping : NSObject
-@end
-@implementation PodsDummy_FastEasyMapping
-@end
--- Pods/Target Support Files/Pods-Network Client/Pods-Network Client-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_Network_ClientVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Network_ClientVersionString[];
-
